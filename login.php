6227b7dcf7e3386332b959e352866745d81c46a5
Thuy <105740492+pnnthuyyyyyy@users.noreply.github.com>
2023-11-05 22:40:31 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IMS Login - Inventory Management System</title>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+    <meta charset="utf-8">
+</head>
+<body id="loginBody">
+	<div class="container">
+		<div class="loginHeader">
+			<h1>OWL</h1>
+			<p>Inventory Management System</p>
+		</div>
+		<div class="loginBody">
+			<form>
+				<div class="loginInputsContainer">
+					<label for="">Mã đăng nhập </label>
+					<input placeholder="username" name="username" type="text" />
+				</div>
+				<div class="loginInputsContainer">
+					<label for="">Mật khẩu</label>
+					<input placeholder="password" name="password" type="password" />
+				</div>
+				<div class="loginButtonContainer">
+					<button>Đăng nhập</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
